f88175911b1ccb61cd855e2f3dc3bddfafda9f9b
Кирилл <artk@gkomega.ru>
2020-09-11 18:40:24 +0300

    create db && UserController.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+import UserController from "./controllers/UserController";
+
 const TelegramBot = require("node-telegram-bot-api");
-const TOKEN = "";
+const express = require("express");
+const Axios = require("axios");
+const mongoose = require("mongoose");
+
+const $PATH =
+  "mongodb+srv://Admin:KCH0E6j5O7kE3eis@cluster0.wdrvv.mongodb.net/spike-weather?retryWrites=true&w=majority";
+const TELEGRAM_TOKEN = "1359387522:AAH261kFyzKmC4vN-mYsvaFfr49ziDTAMwo";
+const WEATHER_TOKEN = "0dd434961d0229f4309f8401b5c244cc";
 
 console.log("Bot has been started...");
 
-const bot = new TelegramBot(TOKEN, {
+mongoose.connect($PATH, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+
+const bot = new TelegramBot(TELEGRAM_TOKEN, {
   polling: {
     interval: 300,
     autoStart: true,
@@ -13,7 +27,14 @@ const bot = new TelegramBot(TOKEN, {
   },
 });
 
+bot.on("polling_error", (m) => console.log(m));
+
 bot.on("message", (message) => {
   const { id } = message.chat;
-  bot.sendMessage(id, `Привет, ${message.from.first_name}!`);
+  const name = message.from.first_name;
+  const text = message.text;
+
+  bot.sendMessage(id, `Привет, ${name}!`);
+  const createUser = new UserController(id);
+  createUser.createUser(name);
 });
